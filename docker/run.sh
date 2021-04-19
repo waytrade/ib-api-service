9aac9b34b522abf127f46e05494c29112b10a7e1
Matthias Frener <mfrener@cinemo.com>
2021-04-19 04:49:07 +0200

    change env var names

--- docker/run.sh
+++ docker/run.sh
@@ -6,7 +6,7 @@ Xvfb :1 -ac -screen 0 1024x768x16 &
 /opt/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" -g \
      "--tws-path=${TWS_PATH}" \
      "--ibc-path=${IBC_PATH}" "--ibc-ini=${IBC_INI}" \
-     "--user=${TWSUSERID}" "--pw=${TWSPASSWORD}" "--mode=${TRADING_MODE}" \
+     "--user=${TWS_USERID}" "--pw=${TWS_PASSWORD}" "--mode=${TRADING_MODE}" \
      "--on2fatimeout=${TWOFA_TIMEOUT_ACTION}" &
 
 sleep 30
