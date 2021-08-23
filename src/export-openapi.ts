431ca4a39ad9ec079c1b3b86d901dc24609228d9
Matthias Frener <mfrener@cinemo.com>
2021-08-23 04:03:09 +0200

    Improve logging on failed app startup.

--- src/export-openapi.ts
+++ src/export-openapi.ts
@@ -1,4 +1,14 @@
 import path from "path";
+import {exit} from "process";
 import {IBApiApp} from "./app";
 
-new IBApiApp().exportOpenApi(path.resolve(__dirname, ".."));
+new IBApiApp()
+  .exportOpenApi(path.resolve(__dirname, ".."))
+  .then(() => {
+    console.info("Successfully exported openapi.json");
+    exit();
+  })
+  .catch(() => {
+    console.error("Failed to export openapi.json");
+    exit(1);
+  });
--- src/run.ts
+++ src/run.ts
@@ -1,3 +1,7 @@
+import {exit} from "process";
 import {IBApiApp} from "./app";
 
-new IBApiApp().start();
+new IBApiApp().start().catch(() => {
+  console.error("Failed to start ib-api-service App");
+  exit(1);
+});
